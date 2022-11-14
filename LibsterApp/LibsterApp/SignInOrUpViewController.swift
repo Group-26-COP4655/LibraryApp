918b0aeab4e2b0c6ddc68c40280837799839a8c9
Geo <geo@Geos-MacBook-Air.local>
2022-11-14 18:31:32 -0500

    initial project layout

---
+++ LibsterApp/LibsterApp/SignInOrUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInOrUpViewController.swift
+//  LibsterApp
+//
+//  Created by Geo on 11/14/22.
+//  Copyright © 2022 Geo. All rights reserved.
+//
+
+import UIKit
+
+class SignInOrUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LibsterApp/LibsterApp/TestViewController.swift
@@ -0,0 +1,61 @@
+//
+//  TestViewController.swift
+//  LibsterApp
+//
+//  Created by Geo on 11/7/22.
+//  Copyright © 2022 Geo. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+//    func testParse2() {
+//        let
+//        User.username = "test_username"
+//        User.password = "test_password"
+//        User.email = "test_email@test.com"
+//        User["userId"] = "A0000"
+//        User["profilePicture"] = "url"
+//        User["bio"] = "lorem ipsem"
+//        User["borrowedBooks"] = []
+//        User["bookmarkds"] = []
+//        User["favorites"] = []
+//        User["friends"] = []
+//
+//        User.signUpInBackground { (Success, Error) in
+//            if (Success) {
+//
+//            }
+//            else {
+//                print("Error!")
+//                print(Error)
+//            }
+//        }
+        
+    
+ 
+        
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
